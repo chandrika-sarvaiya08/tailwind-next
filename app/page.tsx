f7b178760e96f580e9df40e6efca0d0dbfcefc2c
chandrika-sarvaiya08 <chadrikasarvaiya.istosy@gmail.com>
2024-12-17 12:06:02 +0530

    Add

--- app/page.tsx
+++ app/page.tsx
@@ -4,5 +4,6 @@ export default function Home() {
     <div className="flex items-center justify-center h-screen bg-blue-500 text-white">
       <h1 className="text-4xl font-bold">Welcome to Next.js with Tailwind CSS!</h1>
     </div>
+    
   );
 }
